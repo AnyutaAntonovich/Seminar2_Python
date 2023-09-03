f95acf7676f6ce1527f4d2d97abd841951dbbae3
AnyutaAntonovich <antonovich_am@bk.ru>
2023-09-03 13:30:53 +0600

    Add tasks

--- Task1.py
+++ Task1.py
@@ -2,15 +2,15 @@
 # которые нужно перевернуть, чтобы все монетки были повернуты вверх одной и той же стороной. 
 # Выведите минимальное количество монет, которые нужно перевернуть
 
-n = int(input())
+n = int(input('Input the number of coins: '))
 count_zero = 0
 count_one = 0
 for i in range(n):
-    x = int(input())
-if x == 0:
-    count_zero += 1
-else:
-    count_one += 1
+    x = int(input('Input number 1 for heads and number 0 for tails: '))
+    if x == 0:
+        count_zero += 1
+    else:
+        count_one += 1
 if count_one > count_zero:
     print(count_zero)
 else:
